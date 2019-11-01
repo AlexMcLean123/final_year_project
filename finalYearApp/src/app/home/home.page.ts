71d43a9a1cf4404d571382a1191684e25dff1682
AlexMcLean123 <alimclean22@gmail.com>
2019-11-01 00:27:39 +0000

    turned home page into landing page

--- finalYearApp/src/app/home/home.page.ts
+++ finalYearApp/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  [x: string]: any;
 
-  constructor() {
+  constructor(public menuCtrl: MenuController , private router: RouterModule) {
+ 
 
+  }
+
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false);
+  }
+
+  ionViewDidLeave() {
+    // enable the root left menu when leaving the tutorial page
+    this.menuCtrl.enable(true);
   }
 }
